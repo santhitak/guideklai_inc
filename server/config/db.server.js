b24f8c1bd6657ee943669ef22e7f23846379710f
santhitak <santhita.sk@gmail.com>
2022-04-02 12:04:31 +0700

    remove testing password

--- server/config/db.server.js
+++ server/config/db.server.js
@@ -3,7 +3,7 @@ import mysql from "mysql2"
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Pookyl0v3r",
+    password: "",
     database: "webpro",
     waitForConnections: true,
     connectionLimit: 10,
