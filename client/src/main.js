100baf2c853f2191c629e1156ef3f23fbb2b15f9
santhitak <santhita.sk@gmail.com>
2022-04-03 01:25:49 +0700

    reconfig main js

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,14 @@
-import { createApp } from 'vue'
-import router from "./router";
+import { createApp } from 'vue';
+import routes from "./routes";
 import App from './App.vue'
 import DefaultLayout from "./layouts/DefaultLayout.vue";
+import { registerModules } from "@/register-modules";
+import modules from "@/modules";
+
+registerModules(modules)
 
 const app = createApp(App);
 
-app.use(router)
+app.use(routes)
 app.component('layout-nav', DefaultLayout)
 app.mount('#app')
