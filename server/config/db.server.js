67a8111905a6ada7c62fb7d05add3ababce1970f
santhitak <santhita.sk@gmail.com>
2022-04-11 17:56:29 +0700

    rebase host and test validity

--- server/config/db.server.js
+++ server/config/db.server.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2"
 
 const db = mysql.createConnection({
-    host: 'sql6.freesqldatabase.com',
-    user: "sql6485002",
-    password: "BCqsMIKgX4",
-    database: "sql6485002",
+    host: 'mysql8001.site4now.net',
+    user: "a85681_gk",
+    password: "guide123",
+    database: "db_a85681_gk",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
