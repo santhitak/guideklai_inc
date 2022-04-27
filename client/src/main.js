9bb732f62b5b13aaebdbcd08036a42f7bfe3beb4
Sorachat <73382296+ter2002@users.noreply.github.com>
2022-04-28 01:35:04 +0700

    feat: carousel

--- client/src/main.js
+++ client/src/main.js
@@ -6,11 +6,14 @@ import App from "./App.vue";
 import DefaultLayout from "./layouts/DefaultLayout.vue";
 import { registerModules } from "@/register-modules";
 import modules from "@/modules";
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
 registerModules(modules);
 
 const app = createApp(App);
 
 app.use(routes);
+app.use(Antd);
 app.component("layout-nav", DefaultLayout);
 app.mount("#app");
--- client/src/modules/home/views/HomePage.vue
+++ client/src/modules/home/views/HomePage.vue
@@ -1,17 +1,65 @@
+
 <template>
-  <div>
+<div>
+  <!-- <div v-for="article in articles" :key="article.article_id"> -->
+    <div class="">
+      <section style="height: auto">
+
+      <div class="linkToPage" style="
+        display: flex;
+        justify-content:center;
+        gap: 15px;
+        
+      " >
+
+    <LinkBtn>Restaurants</LinkBtn>
+    <LinkBtn>Tour</LinkBtn>
+    <LinkBtn>Guide</LinkBtn>
+    <LinkBtn>Rest</LinkBtn>
+    <LinkBtn>Attraction</LinkBtn>
+    <LinkBtn>Store</LinkBtn>
+      </div>
+      </section>
     <MostViewed />
     <MostRating />
+    <Slider />
+    
+    <!-- <p></p> -->
+    <!-- {{articles}} -->
   </div>
+</div>
 </template>
 
 <script>
 import MostViewed from "@/modules/home/components/MostViewed";
 import MostRating from "@/modules/home/components/MostRating";
-
+import LinkBtn from "@/modules/home/components/LinkBtn";
+import Slider from "@/modules/home/components/Slider";
+// import axios from "axios";
 export default {
   name: "HomePage",
-  components: { MostRating, MostViewed }
+  components: { MostRating, MostViewed, LinkBtn, Slider },
+  data(){
+    return{
+      email: null,
+      password: null,
+      articles:[]
+    }
+  },
+  // created() {
+  //   this.articleData();
+  // }, 
+  // methods: {
+  //   async articleData() {
+  //     try {
+  //       const response = await axios.get("http://localhost:4000/article");
+  //       this.articles = response.data;
+  //       // console.log("🚀 ~ file: Register.vue ~ line 275 ~ memberData ~ this.memberDatas", this.memberDatas)
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   },
+  // },
 };
 </script>
 
