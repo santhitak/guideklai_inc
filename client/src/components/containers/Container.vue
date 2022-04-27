fb33b183738dfb035acda641e7e3d581c0d7fd31
santhitak <santhita.sk@gmail.com>
2022-04-28 02:05:51 +0700

    feat: custom Container.vue

---
+++ client/src/components/containers/Container.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="width: 100vw; height: auto; padding: 3rem 10rem">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "container"
+};
+</script>
