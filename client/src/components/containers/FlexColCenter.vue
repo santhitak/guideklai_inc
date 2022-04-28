f3afef7569a5de317a95e1b89ba77ece5ba842bb
santhitak <santhita.sk@gmail.com>
2022-04-28 22:00:17 +0700

    add: container components

---
+++ client/src/components/containers/FlexColCenter.vue
@@ -0,0 +1,11 @@
+<template>
+  <div style="width: 100%; display: flex; flex-direction: column; justify-content: center; justify-items: center">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FlexColCenter"
+};
+</script>
---
+++ client/src/components/shared/Alert/InfoAlert.vue
@@ -0,0 +1,9 @@
+<template>
+  <a-alert type="info" show-icon style="margin: 1rem 0; width: 50vw"></a-alert>
+</template>
+
+<script>
+export default {
+  name: "InfoAlert"
+};
+</script>
