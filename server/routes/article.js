b1b2ca617534f2e33048d0a21975bb475fee93d8
santhitak <santhita.sk@gmail.com>
2022-04-29 01:15:41 +0700

    fix: readable path

--- server/routes/article.js
+++ server/routes/article.js
@@ -34,7 +34,7 @@ router.get("/article/comment/:id", async function (req, res, next) {
   }
 });
 
-router.get("/article/show/topFiveViewAll", async function (req, res, next) {
+router.get("/article/show/all", async function (req, res, next) {
   try {
     let sql = `SELECT  * FROM article left join promote using(article_id) left join review using(article_id) order by view DESC`;
     const [rows, fields] = await db.query(sql);
@@ -44,17 +44,14 @@ router.get("/article/show/topFiveViewAll", async function (req, res, next) {
   }
 });
 
-router.get(
-  "/article/show/topFiveViewAllReview",
-  async function (req, res, next) {
-    try {
-      let sql = `SELECT  * FROM article left join review using(article_id) left join review_category using(article_id) order by view DESC`;
-      const [rows, fields] = await db.query(sql);
-      return res.json(rows);
-    } catch (err) {
-      return res.status(500).json(err);
-    }
+router.get("/article/show/review/all", async function (req, res, next) {
+  try {
+    let sql = `SELECT  * FROM article left join review using(article_id) left join review_category using(article_id) order by view DESC`;
+    const [rows, fields] = await db.query(sql);
+    return res.json(rows);
+  } catch (err) {
+    return res.status(500).json(err);
   }
-);
+});
 
 exports.router = router;
