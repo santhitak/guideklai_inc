d70b3da5c9d2b36a565561c3c884e2b2cdd487d9
it63070045 <73681882+it63070045@users.noreply.github.com>
2022-04-29 01:29:54 +0700

    fix: delete filterbar

--- client/src/modules/article/views/Article.vue
+++ client/src/modules/article/views/Article.vue
@@ -1,23 +1,19 @@
 <template>
   <section style="height: auto; min-height: 100vh">
-    <div class="flex justify-between">
-      <div class="flex-1">
-        <Filterbar></Filterbar>
-      </div>
-      <div class="flex-auto grow">
+    <div>
+
         <PreviewArticle></PreviewArticle>
-      </div>
+
     </div>
   </section>
 </template>
 
 <script>
-import Filterbar from "@/components/shared/Filter/Filterbar";
 import PreviewArticle from "@/components/shared/PreviewArticle";
 
 export default {
   name: "Article",
-  components: { Filterbar, PreviewArticle },
+  components: { PreviewArticle },
   data() {
     return {};
   },
