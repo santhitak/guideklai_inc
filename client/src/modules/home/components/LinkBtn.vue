af5644763f45ffcd6a9c68fe9626cdd16cd9d24a
santhitak <santhita.sk@gmail.com>
2022-04-29 20:47:39 +0700

    fix: home components

--- client/src/modules/home/components/LinkBtn.vue
+++ client/src/modules/home/components/LinkBtn.vue
@@ -1,7 +1,8 @@
 <template>
   <button
     type="button"
-    class="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800"
+    style="margin: 2rem 0 5rem 0; padding: 1rem 2.5rem"
+    class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
   >
     <slot></slot>
   </button>
--- client/src/modules/home/components/MostViewed.vue
+++ client/src/modules/home/components/MostViewed.vue
@@ -1,12 +1,46 @@
 <template>
   <section>
-    <p class="text-4xl dark:text-white">THIS IS MOST VIEWED CONTENT</p>
+    TOP 5 View in Promote
+    <Slider>
+      <div
+        v-for="article in promoteAllShow.slice(0, 5)"
+        :key="article.article_id"
+      >
+        <h5>{{ article.view }} {{ article.title_promote }}</h5>
+      </div>
+    </Slider>
   </section>
 </template>
 
 <script>
+import axios from "@/plugins/axios";
+
 export default {
-  name: "MostViewed"
+  name: "MostViewed",
+  data() {
+    return {
+      topArticles: []
+    };
+  },
+  computed: {
+    promoteAllShow: function() {
+      return this.topArticles.filter((u) => {
+        return u.type_article === "Promote";
+      });
+    }
+  },
+  methods: {
+    async articleTopFive() {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/article/show/all"
+        );
+        this.topArticles = response.data;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
 };
 </script>
 
