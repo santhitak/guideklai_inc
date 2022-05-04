506414cd85632029982def95691679f3b84af837
santhitak <santhita.sk@gmail.com>
2022-05-04 23:42:10 +0700

    feat: api user

--- server/routes/user.js
+++ server/routes/user.js
@@ -15,4 +15,33 @@ router.put("/manage_account/:member_id", async function (req, res, next) {
   }
 });
 
+router.get(
+  "/manage_account/evidence/:member_id",
+  async function (req, res, next) {
+    try {
+      let results = await db.query(
+        `SELECT evidence_id, image, member_id FROM evidence where member_id = ${req.params.member_id}`
+      );
+      res.send(results);
+    } catch (err) {
+      return res.status(400).json(err);
+    }
+  }
+);
+
+router.post(
+  "/manage_account/evidence/:member_id",
+  async function (req, res, next) {
+    try {
+      let results = await db.query(
+        `INSERT image, member_id FROM evidence where member_id = ${req.params.member_id}`,
+        [req.body.image, req.params.member_id]
+      );
+      res.send(results);
+    } catch (err) {
+      return res.status(400).json(err);
+    }
+  }
+);
+
 exports.router = router;
