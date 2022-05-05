d0d1725a32af5d6efd8f4662c42158a59a05295a
santhitak <santhita.sk@gmail.com>
2022-05-06 03:47:37 +0700

    style: add border

--- client/src/components/shared/Alert/RedAlert.vue
+++ client/src/components/shared/Alert/RedAlert.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800" role="alert">
+  <div
+    class="border border-red-600 p-4 mb-4 text-sm text-red-700 bg-red-100 rounded-lg dark:bg-red-200 dark:text-red-800"
+    role="alert"
+  >
     <slot></slot>
   </div>
 </template>
