4c89ddf4ab1962584454c42aa966c1e252413b6b
santhitak <santhita.sk@gmail.com>
2022-05-05 16:27:09 +0700

    add: user api profile

--- server/routes/user.js
+++ server/routes/user.js
@@ -6,7 +6,8 @@ router.put("/manage_account/:member_id", async function (req, res, next) {
   const email = req.body.email;
   try {
     let results = await db.query(
-      `UPDATE member SET email = ? where member_id = ${req.body.member_id}`,
+      `UPDATE member SET email = ? 
+          where member_id = ${req.body.member_id}`,
       email
     );
     res.send(results);
@@ -20,7 +21,8 @@ router.get(
   async function (req, res, next) {
     try {
       let results = await db.query(
-        `SELECT evidence_id, image, member_id FROM evidence where member_id = ${req.params.member_id}`
+        `SELECT evidence_id, image, member_id FROM evidence
+            where member_id = ${req.params.member_id}`
       );
       res.send(results);
     } catch (err) {
@@ -43,5 +45,46 @@ router.post(
     }
   }
 );
+
+router.get("/article/recent/:member_id", async function (req, res, next) {
+  try {
+    let sql = `SELECT * FROM article a 
+                join member using(member_id) 
+                left join promote using(article_id) 
+                left join review using(article_id) 
+                left join review_category using(article_id) 
+                where member_id = ${req.params.member_id} 
+                order by create_time DESC`;
+
+    const [rows, fields] = await db.query(sql);
+    return res.json(rows);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+router.get("/article/total/:member_id", async function (req, res, next) {
+  try {
+    let sql = `SELECT COUNT(article_id) AS 'total' FROM article
+                where member_id = ${req.params.member_id};`;
+
+    const [rows, fields] = await db.query(sql);
+    return res.json(rows);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+router.get("/comment/total/:member_id", async function (req, res, next) {
+  try {
+    let sql = `SELECT COUNT(comment_id) AS 'total' FROM comment
+                where member_id = ${req.params.member_id};`;
+
+    const [rows, fields] = await db.query(sql);
+    return res.json(rows);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
 exports.router = router;
