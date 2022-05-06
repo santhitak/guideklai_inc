d2433293de03a290f47ca71c045713e2e2c1551b
santhitak <santhita.sk@gmail.com>
2022-05-06 07:52:14 +0700

    add: config in routes

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -30,7 +30,7 @@ const upload = multer({
   storage: storage,
 });
 
-const storage_review = multer.diskStorage({
+const storage_article = multer.diskStorage({
   destination: function (req, file, callback) {
     callback(null, "./uploads/article");
   },
@@ -39,10 +39,10 @@ const storage_review = multer.diskStorage({
   },
 });
 
-const upload_review = multer({
+const upload_article = multer({
   dest: "./uploads/article",
   fileFilter,
-  storage: storage_review,
+  storage: storage_article,
 });
 
 router.post(
@@ -66,7 +66,7 @@ router.post(
 
 router.post(
   "/create/article/review/:member_id",
-  upload_review.array("file", 10),
+  upload_article.array("file", 10),
   isLoggedIn,
   async function (req, res, next) {
     const conn = await db.getConnection();
@@ -109,6 +109,91 @@ router.post(
       return res.status(500).json(err);
     } finally {
       await conn.release();
+    }
+  }
+);
+
+router.post(
+  "/create/article/promote/:member_id",
+  upload_article.array("file", 10),
+  isLoggedIn,
+  async function (req, res, next) {
+    const conn = await db.getConnection();
+    await conn.beginTransaction();
+
+    const type = req.body.type;
+    const language = req.body.language;
+    const province = req.body.province;
+    const price = req.body.price;
+    const file = req.files;
+
+    let pathArray = [];
+
+    try {
+      const [rows1, fields1] = await conn.query(
+        "INSERT INTO article (member_id, information, type_article) VALUES (?, ?, ?)",
+        [req.params.member_id, req.body.information, "Promote"]
+      );
+      const [rows2, fields2] = await conn.query(
+        "INSERT INTO promote (article_id, title_promote, type_promote) VALUES (?, ?, ?)",
+        [rows1.insertId, req.body.title, type]
+      );
+
+      file.forEach((file) => {
+        let path = [rows1.insertId, file.filename];
+        pathArray.push(path);
+      });
+
+      await conn.query("INSERT INTO `images` (article_id, image) VALUES ?;", [
+        pathArray,
+      ]);
+
+      if (type === "Attraction") {
+        const attraction = await conn.query(
+          "INSERT INTO attraction (article_id, province_id, office_hour, attraction_price) VALUES (?, ?, ?, ?)",
+          [rows1.insertId, province, req.body.office_hour, price]
+        );
+      }
+      if (type === "Tour") {
+        const tour = await conn.query(
+          "INSERT INTO tour (article_id, tour_price) VALUES (?, ?)",
+          [rows1.insertId, price]
+        );
+        const tour_province = await conn.query(
+          "INSERT INTO tour_province (article_id, province_id) VALUES (?, ?)",
+          [rows1.insertId, province]
+        );
+        for (let i = 0; i < language.length; i++) {
+          const tour_lang = await conn.query(
+            "INSERT INTO tour_language_skill (article_id, language_id) VALUES (?, ?)",
+            [rows1.insertId, language[i]]
+          );
+        }
+      }
+
+      if (type === "Guide") {
+        const guide = await conn.query(
+          "INSERT INTO guide (article_id, age, gender, guide_price) VALUES (?, ?, ?, ?, )",
+          [rows1.insertId, req.body.age, req.body.gender, price]
+        );
+        for (let i = 0; i < language.length; i++) {
+          const guide_lang = await conn.query(
+            "INSERT INTO guide_language_skill (article_id, language_id) VALUES (?, ?)",
+            [rows1.insertId, language[i]]
+          );
+        }
+      }
+
+      if (type === "Rest") {
+        const guide = await conn.query(
+          "INSERT INTO rest (article_id, province_id, lower_price, higher_price) VALUES (?, ?, ?, ?)",
+          [rows1.insertId, province, req.body.lowerprice, req.body.higherprice]
+        );
+      }
+
+      return res.json(rows1[0]);
+    } catch (err) {
+      return res.status(500).json(err);
     }
   }
 );
